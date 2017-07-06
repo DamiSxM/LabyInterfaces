ee8c88586fae229693001b95a04e8072f470ec66
Dami <dami.bachelet@gmail.com>
2017-07-06 17:34:22 +0200

    Initial commit

---
+++ LabyInterfaces/IAffichage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyInterfaces
+{
+    public interface IAffichage
+    {
+        void PersoMove(int direction);
+        void PersoTeleport(int x, int y);
+
+        void PlayerAdd(string ip, int x, int y);
+        void PlayerMove(string ip, int x, int y);
+        void PlayerRemove(string ip);
+
+        void ItemAdd(int x, int y, string nom);
+        void ItemRemove(int x, int y);
+
+        void Warfog(int lvl);
+    }
+}
